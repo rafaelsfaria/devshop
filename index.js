034c237024076a556a1a2739410d5a7b1c599c3b
Rafael Faria <rafaelsanfaria@gmail.com>
2019-12-26 17:22:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const express = require('express')
+const knex = require('knex')
+const slug = require('./utils/slug')
+const category = require('./models/category')
+const product = require('./models/product')
+
+const db = knex({
+  client: 'mysql2',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'devshop'
+  }
+})
+
+db.on('query', (query) => {
+  console.log('SQL:', query.sql)
+})
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', async (req, res) => {
+  const categories = await category.getCategories(db)()
+  res.render('home', { categories })
+})
+
+app.get('/categoria/:id/:slug', async (req, res) => {
+  const categories = await category.getCategories(db)()
+  const cat = await category.getCategoryById(db)(req.params.id)
+  const products = await product.getProductsByCategoryId(db)(req.params.id)
+  res.render('category', { categories, products, category: cat })
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log('Erro ao iniciar o servidor:', port)
+  } else {
+    console.log('Server running on:', port)
+  }
+})
